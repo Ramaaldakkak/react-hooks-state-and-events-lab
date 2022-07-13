14ed4ee6215a189d28166ceca28d282e08753849
Rama Aldakkak <rama.aldakkak@gmail.com>
2022-07-13 10:32:48 +0300

    Completed assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React from "react"
+import { useState } from "react"
 
-function Item({ name, category }) {
+    // In the Item component, when the user clicks the <button> element, the item should be added to their virtual cart. The way we'll show the user that the item is in the cart is by changing the className on the <li> element:
+    
+export default function Item({ name, category }) {
+
+  const [isInCart, setIsInCart] = useState(false)
+
+  function handleClick() {
+      setIsInCart(!isInCart)
+  }
+
   return (
-    <li className="">
+
+    <li className={isInCart ? 'in-cart' : ''}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleClick} className={isInCart ? 'remove' : 'add'}>{isInCart ? 'Remove' : 'Add'}</button>
     </li>
-  );
+  )
 }
-
-export default Item;
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,33 @@
-import React from "react";
-import Item from "./Item";
+import React from "react"
+import Item from "./Item"
+import { useState } from "react"
 
-function ShoppingList({ items }) {
+
+// Use the useState hook to create a state variable called selectedCategory for keeping track of the selected value from this <select> element. When the value of the <select> element is changed, update state.
+
+// You should also use the selectedCategory variable to determine which items to display in the shopping list. You'll need some way of filtering the array of items based on the selectedCategory.
+
+
+export default function ShoppingList({ items }) {
+
+  const [selectedCategory, setSelectedCategory] = useState("All")
+
+  function handleCategoryChange(event) {
+    setSelectedCategory(event.target.value)
+  }
+
+  const filteredItems = items.filter(item => {
+    if (selectedCategory === "All") {
+      return true
+    } else {
+      return item.category === selectedCategory
+    }
+  })
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleCategoryChange}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,12 +35,10 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
+        {filteredItems.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
     </div>
-  );
+  )
 }
-
-export default ShoppingList;
